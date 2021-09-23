7db52ae850a61d84ce8a1f409b2f57e33f76b2d4
Aneesh Jain <aneeshjain70@gmail.com>
2021-09-23 16:48:50 -0400

    Added train_epoch function

---
+++ main.py
@@ -0,0 +1,78 @@
+from absl import app
+from absl import flags
+from model import SentimentClassifier
+from tqdm import tqdm
+from transformers import get_linear_schedule_with_warmup
+import numpy as np
+import torch
+import transformers
+from transformers import BertModel
+from torch import nn, optim
+from torch.utils import data
+from collections import defaultdict
+import os
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('pre_trained_model_name', 'bert-base-uncased', 'Name of the pre-trained model to use')
+
+
+def train_epoch(
+    model, 
+    data_loader,
+    loss_fn,
+    optimizer,
+    device,
+    scheduler,
+    n_examples,
+    epoch_num, batch_size):
+  
+  model = model.train()
+  losses = []
+  correct_predictions = 0
+  with tqdm(data_loader, unit="batch") as tepoch:
+    for batch in tepoch:
+      
+      tepoch.set_description(f"Epoch {epoch_num}")
+
+      input_ids = batch['input_ids'].to(device)
+      attention_mask = batch['attention_mask'].to(device)
+      labels = batch['label'].to(device)
+
+      # print(input_ids.shape)
+      # print(attention_mask.shape)
+      # print(labels.shape)
+      outputs = model(input_ids = input_ids, attention_mask = attention_mask)
+
+      _, preds = torch.max(outputs, dim = 1)
+
+      loss = loss_fn(outputs, labels)
+
+      batch_correct = torch.sum(preds == labels)
+      correct_predictions += batch_correct 
+      losses.append(loss.item())
+
+      loss.backward()
+      nn.utils.clip_grad_norm_(model.parameters(), max_norm = 1.0)
+      optimizer.step()
+      scheduler.step()
+      optimizer.zero_grad()
+
+      
+
+      tepoch.set_postfix(loss=loss.item(), accuracy=100. * (batch_correct.item()/batch_size))
+  
+  mean_loss = np.mean(losses)
+  accuracy = correct_predictions.double()/n_examples
+  return accuracy, mean_loss
+
+
+
+
+def main(argv):
+
+    model = SentimentClassifier(2)
+
+if __name__ == '__main__':
+  app.run(main)
--- model.py
+++ model.py
@@ -1,11 +1,15 @@
 from torch import nn
 from transformers import BertModel
+from absl import flags
+
+FLAGS = flags.FLAGS
+
 
 class SentimentClassifier(nn.Module):
 
   def __init__(self, n_classes):
     super(SentimentClassifier, self).__init__()
-    self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+    self.bert = BertModel.from_pretrained(FLAGS.pre_trained_model_name)
     self.dropout = nn.Dropout(p = 0.2)
     self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
     self.softmax = nn.Softmax(dim = 1)
